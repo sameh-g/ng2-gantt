d93b25cfef7a11081f8f7498ae9f022264a4da56
Sameh George <sameh.george@itworx.com>
2017-01-11 16:33:44 +0200

    push updated colors ng2 reflections.

--- src/app/gantt-chart/gantt-chart.component.ts
+++ src/app/gantt-chart/gantt-chart.component.ts
@@ -10,15 +10,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class GanttChartComponent implements OnInit {
   color: string = "#000";
   from: string = '';
-  to: string = '50%';
+  to: string = '10%';
 
-  //   color: string = "#000";
-  // from: string = '';
-  // to: string = '50%';
+  color1: string = "#ff0000";
+  from1: string = '';
+  to1: string = '80%';
 
-  //   color: string = "#000";
-  // from: string = '';
-  // to: string = '50%';
+  color2: string = "#7CFC00	";
+  from2: string = '';
+  to2: string = '70%';
+
+  color3: string = "#BDB76B";
+  from3: string = '';
+  to3: string = '40%';
 
   constructor() { }
 
