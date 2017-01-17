5b1249657c90d31f7ef44c3dad4f2f87b6e7a5aa
Sameh George <sameh.george@itworx.com>
2017-01-17 18:02:53 +0200

    update gantt in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {ChartModule} from 'angular2-highcharts'
 import { AppComponent } from './app.component';
 import { GanttChartComponent } from './gantt-chart/gantt-chart.component';
 import { BarchartComponent } from './gantt-chart/barchart/barchart.component';
+import {GanttModule} from './gantt-chart/gantt-chart.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GanttChartComponent,
-    BarchartComponent
+     //GanttChartComponent,
+     //BarchartComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ChartModule
+    ChartModule,
+    GanttModule
   ],
   providers: [],
   bootstrap: [AppComponent]
